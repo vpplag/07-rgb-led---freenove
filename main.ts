ccf3014527767fa60e02dfabd6ebbd8ba2315171
vpplag <74141971+vpplag@users.noreply.github.com>
2021-04-11 14:55:24 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,14 +5,14 @@ function SetColor (red: number, green: number, blue: number) {
     red = Math.map(red, 0, 255, 1023, 0)
     green = Math.map(green, 0, 255, 1023, 0)
     blue = Math.map(blue, 0, 255, 1023, 0)
-    pins.analogWritePin(AnalogPin.P2, red)
+    pins.analogWritePin(AnalogPin.P0, red)
     pins.analogWritePin(AnalogPin.P1, green)
-    pins.analogWritePin(AnalogPin.P0, blue)
+    pins.analogWritePin(AnalogPin.P2, blue)
     basic.pause(200)
 }
 basic.forever(function () {
-    red = randint(0, 255)
-    green = randint(0, 255)
-    blue = randint(0, 255)
+    red = 0
+    green = 0
+    blue = 255
     SetColor(red, green, blue)
 })
